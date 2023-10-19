315d413a14d8780a257eda5b41762069860c4ffd
Davi-web <jeseung.ha@vanderbilt.edu>
2023-10-19 16:43:45 -0700

    Added feature for updating subscription to match with stripe

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -1,4 +1,5 @@
 import {auth} from "@clerk/nextjs"
+import { stripe } from "@/lib/stripe"
 import prismadb from "@/lib/prismadb"
 const DAY_IN_MS = 86_400_000;
 
@@ -20,7 +21,43 @@ export const checkSubscription = async() => {
     })
 
     if(!userSubscription) return false
-    const isValid = userSubscription.stripePriceId && userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
 
-    return !!isValid
+    const isValid = userSubscription.stripePriceId && userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+    if(!!isValid) {
+        return true
+    } else{
+        // If the subscription is not valid, check if the user has a valid subscription in Stripe
+        if(userSubscription.stripeSubscriptonId) {
+            const stripeSubscription = await stripe.subscriptions.retrieve(userSubscription.stripeSubscriptonId, {
+                expand:["latest_invoice", "plan"]
+            }, {
+                maxNetworkRetries: 3
+            })
+            if(stripeSubscription.status === "active") {
+                await prismadb.userSubscription.update({
+                    where: {
+                        userId
+                    },
+                    data: {
+                        stripeCurrentPeriodEnd: new Date(stripeSubscription.current_period_end * 1000),
+                    }
+                })
+                return true
+            } else {
+                // If the subscription is not active in Stripe, remove the subscription from the database
+                await prismadb.userSubscription.update({
+                    where: {
+                        userId
+                    },
+                    data: {
+                        stripeSubscriptonId: null,
+                        stripeCurrentPeriodEnd: null,
+                    }
+                })
+                return false
+            }
+        }
+    }
+
+    return false
 }
